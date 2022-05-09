6f14cf3fed2e2614b33fce2c9ae9bd53127f3774
Ji Park <jiyongprk@gmail.com>
2022-05-08 22:20:28 -0400

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,7 @@
 export default function Footer() {
     return (
         <footer className='footer-container bg-dark text-light'>
-            <p>f</p>
-            <p>f</p>
-            <p>f</p>
-            <p>f</p>
+            <p>Created by <a href="https://github.com/norfkorean/react-crypto-news-live-app">Ji Park</a></p>
         </footer>
     )
 }
